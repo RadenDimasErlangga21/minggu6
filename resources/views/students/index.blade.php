eebfbc92729421bda1f28cd082131194d556bf0a
RadenDimasErlangga21 <zirkified123@gmail.com>
2021-10-14 15:02:38 +0700

    menampilkan data students

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">{{ __('STUDENT DATA') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+
+                    <a href="/students/create" class="btn btn-primary">Add Data</a> <br><br>
+                    <table class="table table-responsive table-striped">
+                        <thead>
+                            <tr>
+                                <th>NIM</th>
+                                <th>Name</th>
+                                <th>Class</th>
+                                <th>Department</th>
+                                <th>Action</th> 
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($student as $s)
+                            <tr>
+                                <td>{{ $s->nim }}</td>
+                                <td>{{ $s->name }}</td>
+                                <td>{{ $s->class }}</td>
+                                <td>{{ $s->department }} </td>
+                                <td>
+                                    <form action="/students/{{$s->id}}" method="post">
+                                        <a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a>
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" name="delete" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
